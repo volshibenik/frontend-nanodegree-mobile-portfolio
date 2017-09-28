0086c4de698a9773fa660ef803307a08624eb448
volshibenik <volshibenik@gmail.com>
2017-09-29 00:40:51 +0300

    test

--- sw.js
+++ sw.js
@@ -1,32 +1,23 @@
-const VERSION = "3.0.1";
 
-// const cacheName = 'techqueria-org';
-const APP_CACHE_NAME = 'techqueria-org-app';
-const STATIC_CACHE_NAME = 'techqueria-org-static';
 
 console.log(`installing sw.js`);
 
 const CACHE_STATIC = [
 
-  '/css/print.css'
+  'css/print.css'
 ];
 
 const CACHE_APP = [
   '/',
   '/index.html',
-  '/about/',
-  '/contact/',
-  '/events/',
-  '/code-of-conduct/',
-  '/merch/',
-  '/welcome-to-slack/',
+
 ];
 
 self.addEventListener('install',function(e){
   e.waitUntil(
     Promise.all([
-      caches.open(STATIC_CACHE_NAME),
-      caches.open(APP_CACHE_NAME),
+      caches.open(CACHE_STATIC),
+      caches.open(CACHE_APP),
       self.skipWaiting()
     ]).then(function(storage){
       var static_cache = storage[0];
@@ -45,7 +36,7 @@ self.addEventListener('activate', function(e) {
       caches.keys().then(function(cacheNames) {
         return Promise.all(
           cacheNames.map(function(cacheName) {
-            if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
+            if (cacheName !== CACHE_APP && cacheName !== CACHE_STATIC) {
               console.log('deleting',cacheName);
               return caches.delete(cacheName);
             }
@@ -66,7 +57,7 @@ this.addEventListener('fetch', function(event) {
   })
                     .then(function(r) {
     response = r;
-    caches.open(cacheName).then(function(cache) {
+    caches.open(CACHE_STATIC).then(function(cache) {
       cache.put(event.request, response);
     });
     return response.clone();
